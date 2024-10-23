dced7ef59283b149f691450c21d52c016eb70385
Thomas Philip <thomasphilipchry@gmail.com>
2024-10-23 10:51:58 +0530

    Refactored code

---
+++ src/main/java/com/example/EmployeeManagementSystem/validator/EmployeeValidator.java
@@ -0,0 +1,15 @@
+package com.example.EmployeeManagementSystem.validator;
+
+import com.example.EmployeeManagementSystem.entity.Employee;
+
+import java.util.Objects;
+
+public class EmployeeValidator {
+
+    public static boolean validEmployee(Employee employee) {
+         return !Objects.isNull(employee.getFirstName()) && !Objects.isNull(employee.getLastName()) &&
+                !Objects.isNull(employee.getEmail()) && !Objects.isNull(employee.getDepartment()) &&
+                !Objects.isNull(employee.getJobTitle()) && !Objects.isNull(employee.getStatus()) &&
+                 employee.getFirstName().length() < 10 && employee.getLastName().length() < 10;
+    }
+}
